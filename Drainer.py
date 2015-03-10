70c56ab2b88cccada662c097044a7f0193afc606
Alejandro Fdez Carrera <alej4fc@gmail.com>
2015-03-10 11:37:56 +0100

    update core with api rest and refactored code

--- Drainer.py
+++ Drainer.py
@@ -1,30 +1,39 @@
 
-__author__ = 'Alejandro F. Carrera'
+__author__ = 'alejandrofcarrera'
 
 from flask import request, make_response, Flask
-from flask_negotiate import consumes
-from gitlab import system
+from flask_negotiate import consumes, produces
+import glmodule
+import json
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 
+drainer = glmodule.GlDrainer(app.config)
+
 @app.route('/system', methods=['POST'])
 @consumes('application/json')
 def hook_system():
-    if app.config['DEBUGGER']:
+    if app.config.get('DEBUGGER', True):
         print('System event: %s' % request.data)
-    system.hook_system(request.json)
+    drainer.hook_system(request.json)
     resp = make_response('', 204)
     return resp
 
 @app.route('/hook', methods=['POST'])
 @consumes('application/json')
 def hook_specific():
-    if app.config['DEBUGGER']:
+    if app.config.get('DEBUGGER', True):
         print('Hook event: %s' % request.data)
-    # system.hook_specific(request.json)
+    drainer.hook_specific(request.json)
     resp = make_response('', 204)
     return resp
 
+@app.route('/api/<name>', methods=['GET'])
+@produces('application/json')
+def api_gitlab(name):
+    resp = make_response(json.dumps(drainer.api_gitlab(name)), 200)
+    return resp
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(app.config.get('DRAINER_LISTEN_IP', '0.0.0.0'), app.config.get('DRAINER_PORT', 5000))
