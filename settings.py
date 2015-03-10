98f22db350ed82f43ddf2645375d0fabd096aaee
Alejandro Fdez Carrera <alej4fc@gmail.com>
2015-03-10 11:36:31 +0100

    update settings file

--- settings.py
+++ settings.py
@@ -1,7 +1,15 @@
 
-__author__ = 'Alejandro F. Carrera'
+__author__ = 'alejandrofcarrera'
 
-DEBUGGER = False
+DEBUGGER = True
 
-DRAINER_IP = 'http://10.2.2.2'
-DRAINER_PORT = '5000'
+DRAINER_PROT = 'http'
+DRAINER_IP = '10.0.2.2'
+DRAINER_PORT = 5000
+DRAINER_LISTEN_IP = '0.0.0.0'
+
+GITLAB_PROT = 'http'
+GITLAB_IP = '127.0.0.1'
+GITLAB_PORT = 8000
+GITLAB_USER = 'root'
+GITLAB_PASS = '12345678'
