e62161bd0ceaca1ae673f16528a0cf55c45d2ff8
Alejandro Fdez Carrera <alej4fc@gmail.com>
2015-07-28 17:03:15 +0200

    change all settings to environ properties

--- drainer/settings.py
+++ drainer/settings.py
@@ -26,15 +26,15 @@ import os
 DEBUGGER = True
 
 DRAINER_PROT = 'http'
-DRAINER_IP = '10.0.2.2'
-DRAINER_PORT = 5000
-DRAINER_LISTEN_IP = '0.0.0.0'
+DRAINER_IP = os.environ.get("DRAINER_IP", "10.0.2.2")
+DRAINER_PORT = int(os.environ.get("DRAINER_PORT", 5000))
+DRAINER_LISTEN_IP = os.environ.get("DRAINER_LISTEN_IP", "0.0.0.0")
 
 GITLAB_PROT = 'http'
 GITLAB_IP = os.environ.get("ENH_GITLAB_IP", "127.0.0.1")
 GITLAB_PORT = int(os.environ.get("ENH_GITLAB_PORT", 8000))
-GITLAB_USER = os.environ.get("ENH_GITLAB_USER", 'root')
-GITLAB_PASS = os.environ.get("ENH_GITLAB_PASS", '12345678')
+GITLAB_USER = os.environ.get("ENH_GITLAB_USER", "root")
+GITLAB_PASS = os.environ.get("ENH_GITLAB_PASS", "12345678")
 
 # GITLAB_PASS = '5iveL!fe'
 
